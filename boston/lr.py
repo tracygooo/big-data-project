bb7cb59fffeeb8d96f6c72192443d1d765cb712a
tracygooo <tracygooo@gmail.com>
2018-03-27 15:27:52 -0400

    Rearranged data storage after outputing the data needed for DMak software

--- boston/lr.py
+++ boston/lr.py
@@ -7,27 +7,30 @@ Used to check results of hadoop streaming
 '''
 
 import numpy as np
+from sklearn import preprocessing
 from numpy.linalg import inv
 
 # Read data to matrix 'mat' from 'boston.pat' 
-count = 0
 f = open( 'input/boston.pat' , 'r' )
 mat = np.loadtxt( f )
 row = mat.shape[ 0 ]
 
 # X: first 13 columns 
-X = mat[ : , :13 ]
+X_org = mat[ : , :13 ]
+X_scaled = preprocessing.scale( X_org )
+#xscaler = preprocessing.StandardScaler().fit( X_org )
 
 # Add last column with ones for bias (omega_0)
 X0 = np.ones( row )
 X0 = np.array( [ X0 ] )
-X = np.concatenate( ( X , X0.T ) , axis = 1 )
+X = np.concatenate( ( X_scaled , X0.T ) , axis = 1 )
 
 # Y_real: 14th column (15th (last) column is ID)
 Y_real = mat[ : , 13 ]
+Y_real_scaled = preprocessing.scale( Y_real )
 
 # Convert Y-real to 2-D array
-Y_real = np.array( [ Y_real ] )
+Y_real = np.array( [ Y_real_scaled ] )
 
 XTX = np.matmul( X.T , X )
 XTY =  np.matmul(  X.T , Y_real.T )
